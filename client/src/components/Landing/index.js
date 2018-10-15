fe884759de968ded4d233a89826385b0ea2423f2
Vinh Nguyen <vinh_nguyen1211@yahoo.com.vn>
2018-10-15 23:02:15 +0700

    tweak component state

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -1,80 +1,86 @@
 import React, { Component } from 'react';
+import { Query } from 'react-apollo'
 import { GET_ALL_USERS } from '../Session/queries'
 import withSession from '../Session/withSession';
 import { MessageCreate, Messages } from '../Message';
 import { client } from '../../'
 
-const Landing = ({ session }) => {
+const Landing = () => {
   // console.log('session: ',session)
   return(
     <div>
-      <h2>Landing Page</h2>
-      {session && session.me && <MessageCreate />}
-      <Messages me={session.me} limit={2} />
+      <Page />
       <AllUsers />
     </div>
   )
 };
 
-// const AllUsers = () => (
-//   <Query query={GET_ALL_USERS} >
-//      {({data, loading, error, refetch}) => {
-//       if(!data){
-//         return <div>No users to be shown</div>
-//       }
-//       return(
-//         <div>{data && data.users && data.users.map(user => 
-//           <ul key={user.id} >
-//             <li>UserId: {user.id}</li>
-//             <li>Username: {user.username}</li>
-//             <li>Email: {user.email}</li>
-//           </ul>
-//         )}
-//         </div>
-//       )
-//     }}
-//   </Query>
-// )
+const AllUsers = () => (
+  <Query query={GET_ALL_USERS} >
+     {({data, loading, error, refetch}) => {
+      if(loading){
+        return(<div>Loading users....</div>)
+      }
+      if(!data || (data.users && data.users.length === 0)){
+        return <div>No users to be shown</div>
+      }
+      return(
+        <div>{data && data.users && data.users.map(user => 
+          <ul key={user.id} >
+            <li>UserId: {user.id}</li>
+            <li>Username: {user.username}</li>
+            <li>Email: {user.email}</li>
+          </ul>
+        )}
+        </div>
+      )
+    }}
+  </Query>
+)
 
-const query = `
-  query users{
-    id,
-    username,
-    email,
-    role,
-    messages{
-      text,
-      createdAt
-    }
-  }
-`
+const _Page = ({session}) => (
+  <div>
+    <h2>Landing Page</h2>
+      {session && session.me && <MessageCreate />}
+      <Messages me={session.me} limit={2} />
+  </div>
+)
 
-class AllUsers extends Component{
+const Page = withSession(_Page)
 
-  componentDidMount = async () => {
-    let result
-    try {
-      result = await client.query({query: GET_ALL_USERS})
-    } catch (error) {
-      // console.log('error: ' ,error);
-    }
-    console.log('result: ',result);
-  }
+// class AllUsers extends Component{
 
-  render() {
-    const data = []
+//   state = {
+//     users: []
+//   }
 
-    return (
-      <div>{data && data.users && data.users.map(user => 
-        <ul key={user.id} >
-          <li>UserId: {user.id}</li>
-          <li>Username: {user.username}</li>
-          <li>Email: {user.email}</li>
-        </ul>
-      )}
-      </div>
-    )
-  }
-}
+//   componentDidMount = async () => {
+//     let result
+//     try {
+//       const { data: { users }} = await client.query({query: GET_ALL_USERS})
+//       this.setState({users})
+//     } catch (error) {
+//       // const { graphQLErrors: errors } = error
+//       // const msg = errors.map( item => item.message).join(', ')
+//       // console.log('msg: ' ,msg);
+//     }
 
-export default withSession(Landing);
+//   }
+
+//   render() {
+//     const { users } = this.state
+  
+//     return (
+//       <div>{users && users.map(user => 
+//         <ul key={user.id} >
+//           <li>UserId: {user.id}</li>
+//           <li>Username: {user.username}</li>
+//           <li>Email: {user.email}</li>
+//         </ul>
+//       )}
+//       </div>
+//     )
+//   }
+// }
+
+export default Landing
