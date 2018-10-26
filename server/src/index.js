a368a16c5f1171c635bf2c1a29af052e08cc51db
Vinh Nguyen <vinh_nguyen1211@yahoo.com.vn>
2018-10-26 20:50:12 +0700

    docker compose done

--- server/src/index.js
+++ server/src/index.js
@@ -13,7 +13,7 @@ import resolvers from './resolvers';
 import models, { sequelize } from './models';
 import loaders from './loaders';
 
-const port = process.env.PORT || 5000;
+const port = process.env.SERVER_PORT || 5000;
 
 const app = express();
 
@@ -21,7 +21,7 @@ const corsOptions = {
   origin: `http://localhost:${port}`,
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'html');
 
