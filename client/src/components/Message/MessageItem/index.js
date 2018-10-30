0024500dde7bca268c8f8c64e51196d2b329b250
Vinh Nguyen <vinh_nguyen1211@yahoo.com.vn>
2018-10-30 08:23:24 +0700

    fix message

--- client/src/components/Message/MessageItem/index.js
+++ client/src/components/Message/MessageItem/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Editor, convertFromRaw, EditorState } from 'draft-js';
+import { Editor as EditorWysiwyg} from 'react-draft-wysiwyg'
+import { convertFromRaw, EditorState } from 'draft-js';
 import MessageDelete from '../MessageDelete';
 
 class MessageItem extends Component{
@@ -10,8 +11,8 @@ class MessageItem extends Component{
 
   render(){
     const { message, me } = this.props
-    console.log('message: ' ,message);
-    const messageShow = EditorState.createWithContent(
+    // console.log('message: ' ,message);
+    const messageShow = message && EditorState.createWithContent(
       convertFromRaw(JSON.parse(message.text))
     );
 
@@ -20,7 +21,7 @@ class MessageItem extends Component{
         <h3>{message.user.username}</h3>
         <small>{message.createdAt}</small>
         <div style={{maxWidth: 800, border: '1px solid #448aff', marginBottom: 12}} >
-          <Editor editorState={messageShow} readOnly/>
+          <EditorWysiwyg editorState={messageShow} readOnly toolbarHidden />
         </div>
 
         {me &&
